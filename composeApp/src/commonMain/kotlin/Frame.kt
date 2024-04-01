2bc531db1d42cd217193dac83e15ca3c284f3acf
me <coderwj@live.com>
2024-04-01 14:23:21 +0800

    实现简单的布局和界面切换

---
+++ composeApp/src/commonMain/kotlin/Frame.kt
@@ -0,0 +1,54 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Button
+import androidx.compose.material.Card
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import json.JsonTool
+
+@Composable
+fun Frame() {
+    val pages = listOf("JsonTool", "Debug")
+    Card {
+        var page by remember { mutableStateOf(pages.first()) }
+
+        Column(
+            modifier = Modifier,
+            verticalArrangement = Arrangement.spacedBy(4.dp)
+        ) {
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                horizontalArrangement = Arrangement.SpaceAround
+            ) {
+                pages.forEach { pageName ->
+                    Button(onClick = { page = pageName }) {
+                        Text(pageName)
+                    }
+                }
+            }
+
+            if (page == "JsonTool") {
+                JsonTool()
+            } else {
+                Debug()
+            }
+        }
+    }
+}
+
+@Composable
+fun Debug() {
+
+    Card {
+        Text("debug page...")
+    }
+
+}
