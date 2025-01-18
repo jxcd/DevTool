4eeb3f2375d848bddda537f2ee7d4c9a5f678fea
me <coderwj@live.com>
2025-01-18 19:15:30 +0800

    调整使用 Scaffold 布局

--- composeApp/src/commonMain/kotlin/Frame.kt
+++ composeApp/src/commonMain/kotlin/Frame.kt
@@ -1,10 +1,23 @@
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.material.Button
 import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.OutlinedButton
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.automirrored.filled.List
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -16,32 +29,60 @@ import pages.JsonTool
 import pages.RegexTool
 import pages.TimeTool
 
+const val mainPage = "MainPage"
+val pages = listOf(mainPage, "JsonTool", "TimeTool", "RegexTool", "Debug")
+
+
 @Composable
 fun Frame() {
-    val pages = listOf("JsonTool", "TimeTool", "RegexTool", "Debug")
-    Card {
-        var page by remember { mutableStateOf(pages.first()) }
+    var page by remember { mutableStateOf(mainPage) }
 
-        Column(
-            modifier = Modifier,
-            verticalArrangement = Arrangement.spacedBy(4.dp)
-        ) {
-            Row(
-                modifier = Modifier.fillMaxWidth(),
-                horizontalArrangement = Arrangement.SpaceAround
-            ) {
-                pages.forEach { pageName ->
-                    Button(onClick = { page = pageName }) {
-                        Text(pageName)
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(page) },
+                navigationIcon = {
+                    IconButton(onClick = { page = mainPage }, enabled = page != mainPage) {
+                        Icon(
+                            imageVector =
+                            if (page == mainPage) Icons.AutoMirrored.Filled.List
+                            else Icons.AutoMirrored.Filled.ArrowBack,
+                            contentDescription = "Back"
+                        )
                     }
                 }
-            }
-
+            )
+        }
+    ) {
+        Card(
+            modifier = Modifier.fillMaxWidth().padding(top = 8.dp)
+        ) {
             when (page) {
+                "MainPage" -> MainPage { page = it }
                 "JsonTool" -> JsonTool()
                 "TimeTool" -> TimeTool()
                 "RegexTool" -> RegexTool()
                 else -> Debug()
+            }
+        }
+    }
+}
+
+@Composable
+fun MainPage(setPage: (String) -> Unit) {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        verticalArrangement = Arrangement.spacedBy(16.dp),
+        horizontalArrangement = Arrangement.spacedBy(16.dp)
+    ) {
+        pages.filter { it != mainPage }.forEach { pageName ->
+            item {
+                OutlinedButton(
+                    onClick = { setPage(pageName) },
+                    modifier = Modifier.width(24.dp).padding(8.dp)
+                ) {
+                    Text(pageName)
+                }
             }
         }
     }
